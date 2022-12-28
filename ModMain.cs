12f1c77256abf4524c335d06d48656a04fcafcb4
silentstorm <what@what.com>
2022-12-28 18:45:17 +1100

    been a while, hasn't it?

--- ModMain.cs
+++ ModMain.cs
@@ -1,121 +1,229 @@
-﻿//saves so much space with global using, no need to put separate usings in each file, just put them all in the main one
-global using Assets.Scripts.Models;
-global using Assets.Scripts.Models.Towers;
-global using Assets.Scripts.Models.GenericBehaviors;
-global using Assets.Scripts.Models.Map;
-global using Assets.Scripts.Unity.Display;
-global using Assets.Scripts.Utils;
-global using Assets.Scripts.Models.Towers.Behaviors.Attack;
-global using Assets.Scripts.Models.Towers.Behaviors;
-global using Assets.Scripts.Models.Towers.Behaviors.Emissions;
-global using Assets.Scripts.Models.Towers.Projectiles.Behaviors;
-global using Assets.Scripts.Unity;
-global using Assets.Scripts.Models.Towers.Behaviors.Abilities;
-global using Assets.Scripts.Models.Towers.Behaviors.Abilities.Behaviors;
-global using Assets.Scripts.Simulation.Towers;
-global using Assets.Scripts.Models.Towers.Behaviors.Attack.Behaviors;
-global using Assets.Scripts.Models.Towers.TowerFilters;
-global using Assets.Scripts.Simulation.Towers.Weapons;
-global using Assets.Scripts.Models.Towers.Filters;
-global using Assets.Scripts.Simulation.Towers.Behaviors.Abilities;
-global using Assets.Scripts.Unity.Audio;
-global using Assets.Scripts.Models.Bloons.Behaviors;
-global using Assets.Scripts.Models.Towers.Weapons.Behaviors;
-global using BTD_Mod_Helper;
-global using BTD_Mod_Helper.Extensions;
-global using BTD_Mod_Helper.Api.Towers;
-global using BTD_Mod_Helper.Api.ModOptions;
-global using UnhollowerBaseLib;
-global using static SC2Expansion.SC2Expansion;
-global using System.Linq;
-global using uObject=UnityEngine.Object;
-global using HarmonyLib;
-global using MelonLoader;
-global using System.Collections.Generic;
-global using UnityEngine;
-global using UnityEngine.UI;
-global using System;
-[assembly:MelonGame("Ninja Kiwi","BloonsTD6")]
-[assembly:MelonInfo(typeof(SC2Expansion.SC2Expansion),"SC2Expansion","1.5.4","Silentstorm#5336")]
-namespace SC2Expansion{
-    public class SC2Expansion:BloonsTD6Mod{
-        [HarmonyPatch(typeof(GameModelLoader),"Load")]
-        public static class GameModelLoaderLoad_Patch{
+﻿[assembly:MelonGame("Ninja Kiwi","BloonsTD6")]
+[assembly:MelonInfo(typeof(SC2ExpansionLoader.ModMain),ModHelperData.Name,ModHelperData.Version,"Silentstorm")]
+namespace SC2ExpansionLoader{
+    public class ModMain:MelonMod{
+        public static Dictionary<string,SC2Tower>TowerTypes=new Dictionary<string,SC2Tower>();
+        public static AbilityModel BlankAbilityModel;
+        private static MelonLogger.Instance mllog;
+        public static string BundleDir;
+        public static void Log(object thingtolog,string type="msg"){
+            switch(type){
+                case"msg":
+                    mllog.Msg(thingtolog);
+                    break;
+                case"warn":
+                    mllog.Warning(thingtolog);
+                    break;
+                 case"error":
+                    mllog.Error(thingtolog);
+                    break;
+            }
+        }
+        [HarmonyPatch(typeof(Btd6Player),"CheckForNewParagonPipEvent")]
+        public class Btd6PlayerCheckForNewParagonPipEvent_Patch{
+            [HarmonyPrefix]
+            public static bool Prefix(){
+                return false;
+            }
+        }
+        [HarmonyPatch(typeof(ProfileModel),"Validate")]
+        public class ProfileModelValidate_Patch{
             [HarmonyPostfix]
-            public static void Postfix(ref GameModel __result){
-                if(RemoveBaseTowers==true){
-                    __result.towerSet=__result.towerSet.Empty();
+            public static void Postfix(ProfileModel __instance){
+                foreach(string tower in TowerTypes.Keys){
+                    __instance.unlockedTowers.Add(tower);
+                    foreach(UpgradeModel upgrade in TowerTypes[tower].Upgrades()){
+                        __instance.acquiredUpgrades.Add(upgrade.name);
+                    }
                 }
             }
         }
-        //i should really test if actually reads all of the soundmodel's here
-        public static void SetUpgradeSounds(TowerModel towerModel,string soundToUse){
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound.assetId=soundToUse;
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound1.assetId=soundToUse;
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound2.assetId=soundToUse;
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound3.assetId=soundToUse;
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound4.assetId=soundToUse;
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound5.assetId=soundToUse;
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound6.assetId=soundToUse;
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound7.assetId=soundToUse;
-            towerModel.GetBehavior<CreateSoundOnUpgradeModel>().sound8.assetId=soundToUse;
+        public static void SaveIl2CppObj(string fileName,Il2CppSystem.Object obj){
+            System.IO.File.WriteAllText(fileName,JsonConvert.SerializeObject(obj,Formatting.Indented));
         }
-        public static void LoadImage(AssetBundle assetBundle,string asset,Image image){
-            var text=assetBundle.LoadAsset(asset).Cast<Texture2D>();
-            image.canvasRenderer.SetTexture(text);
-            image.sprite=Sprite.Create(text,new(0,0,text.width,text.height),new());
+        public override void OnInitializeMelon(){
+            mllog=LoggerInstance;
+            BundleDir=MelonEnvironment.UserDataDirectory+"/SC2ExpansionBundles/";
+            Directory.CreateDirectory(BundleDir);
+            foreach(MelonMod mod in RegisteredMelons){
+                if(mod.Info.Name.StartsWith("SC2Expansion")){
+                    Assembly assembly=mod.MelonAssembly.Assembly;
+                    foreach(Type type in assembly.GetTypes()){
+                        try{
+                            SC2Tower tower=(SC2Tower)Activator.CreateInstance(type);
+                            if(tower.Name!=""){
+                                TowerTypes.Add(tower.Name,tower);
+                                TowerTypes[tower.Name].LoadedBundle=UnityEngine.AssetBundle.LoadFromFileAsync(BundleDir+tower.Name.ToLower()).assetBundle;
+                            }
+                        }catch{}
+                    }
+                    foreach(string bundle in assembly.GetManifestResourceNames()){
+                        Stream stream=assembly.GetManifestResourceStream(bundle);
+                        byte[]bytes=new byte[stream.Length];
+                        stream.Read(bytes);
+                        File.WriteAllBytes(BundleDir+bundle.Split('.')[2],bytes);
+                    }                     
+                }
+            }
         }
-        public static bool LoadModel(AssetBundle assetBundle,string asset,string check,Factory factory,Il2CppSystem.Action<UnityDisplayNode>DisplayAction){
-            if(asset.StartsWith(check)){
-                if(!DisplayDict.ContainsKey(asset)){
-                    var udn=uObject.Instantiate(assetBundle.LoadAsset(asset).Cast<GameObject>(),factory.PrototypeRoot).AddComponent<UnityDisplayNode>();
-                    udn.transform.position=new(-30000,-30000);
-                    udn.name=asset;
-                    udn.isSprite=false;
-                    DisplayAction.Invoke(udn);
-                    DisplayDict.Add(asset,udn);
+        public static uObject LoadAsset<T>(string Asset,AssetBundle Bundle){
+            try{
+                return Bundle.LoadAssetAsync(Asset,Il2CppType.Of<T>()).asset;
+            }catch{
+                foreach(KeyValuePair<string,SC2Tower>tower in TowerTypes){
+                    tower.Value.LoadedBundle=UnityEngine.AssetBundle.LoadFromFileAsync(BundleDir+tower.Key.ToLower()).assetBundle;
+                    /*foreach(var thing in tower.Value.LoadedBundle.GetAllAssetNames()){
+                        Log(thing);
+                    }*/
+                }
+                return Bundle.LoadAssetAsync(Asset,Il2CppType.Of<T>()).asset;
+            }
+        }
+        [HarmonyPatch(typeof(TitleScreen),"Start")]
+        public class TitleScreenStart_Patch{
+            [HarmonyPostfix]
+            public static void Postfix(){
+                BlankAbilityModel=Game.instance.model.GetTowerFromId("Quincy 4").Cast<TowerModel>().behaviors.
+                    First(a=>a.GetIl2CppType().Name=="AbilityModel").Clone().Cast<AbilityModel>();
+                BlankAbilityModel.description="AbilityDescription";
+                BlankAbilityModel.displayName="AbilityDisplayName";
+                BlankAbilityModel.name="AbilityName";
+                List<Model>behaviors=BlankAbilityModel.behaviors.ToList();
+                behaviors.Remove(behaviors.First(a=>a.GetIl2CppType().Name=="TurboModel"));
+                behaviors.Remove(behaviors.First(a=>a.GetIl2CppType().Name=="CreateEffectOnAbilityModel"));
+                behaviors.Remove(behaviors.First(a=>a.GetIl2CppType().Name=="CreateSoundOnAbilityModel"));
+                BlankAbilityModel.behaviors=behaviors.ToArray();
+                List<TowerModel>towers=Game.instance.model.towers.ToList();
+                List<TowerDetailsModel>towerSet=Game.instance.model.towerSet.ToList();
+                List<UpgradeModel>upgrades=Game.instance.model.upgrades.ToList();
+                List<string>towerNames=new();
+                try{
+                    foreach(SC2Tower tower in TowerTypes.Values){
+                        towerNames.Add(tower.Name);
+                        foreach(TowerModel towerModel in tower.TowerModels()){
+                            Log(1);
+                            towers.Add(towerModel);
+                            Log(2);
+                        }
+                        towerSet.Add(tower.ShopDetails());
+                        foreach(UpgradeModel upgrade in tower.Upgrades()){
+                            upgrades.Add(upgrade);
+                        }
+                        Game.instance.model.towers=towers.ToArray();
+                        Game.instance.model.towerSet=towerSet.ToArray();
+                        Game.instance.model.upgrades=upgrades.ToArray();
+                        Log("Loaded "+tower.Name);
+                    }
+                }catch{
+                    Log("Failed to add "+towerNames.Last());
+                }
+            }
+        }
+        [HarmonyPatch(typeof(Factory.__c__DisplayClass21_0),"_CreateAsync_b__0")]
+        public class FactoryCreateAsync_Patch{
+            [HarmonyPrefix]
+            public static bool Prefix(ref Factory.__c__DisplayClass21_0 __instance,ref UnityDisplayNode prototype){
+                string towerName=__instance.objectId.guidRef.Split('-')[0];
+                if(towerName!=null&&TowerTypes.ContainsKey(towerName)){
+                    SC2Tower tower=TowerTypes[towerName];
+                    GameObject gObj=uObject.Instantiate(LoadAsset<GameObject>(__instance.objectId.guidRef,tower.LoadedBundle).Cast<GameObject>(),
+                        __instance.__4__this.DisplayRoot);
+                    gObj.name=__instance.objectId.guidRef;
+                    gObj.transform.position=new(0,0,30000);
+                    gObj.AddComponent<UnityDisplayNode>();
+                    gObj.AddComponent<SC2Sound>();
+                    prototype=gObj.GetComponent<UnityDisplayNode>();
+                    __instance.__4__this.active.Add(prototype);
+                    __instance.onComplete.Invoke(prototype);
                     return false;
                 }
-                if(DisplayDict.ContainsKey(asset)){
-                    DisplayAction.Invoke(DisplayDict[asset]);
-                    return false;
+                return true;                
+            }
+        }
+        [HarmonyPatch(typeof(UnityEngine.U2D.SpriteAtlas),"GetSprite")]
+        public class SpriteAtlasGetSprite_Patch{
+            [HarmonyPostfix]
+            public static void Postfix(string name,ref Sprite __result){
+                string towerName="";
+                try{
+                    towerName=name.Split('-')[0];
+                }catch{}
+                if(TowerTypes.ContainsKey(towerName)){
+                    SC2Tower tower=TowerTypes[towerName];
+                    Texture2D texture=LoadAsset<Texture2D>(name,tower.LoadedBundle).Cast<Texture2D>();
+                    __result=Sprite.Create(texture,new(0,0,texture.width,texture.height),new());
                 }
             }
-            return true;
         }
-        public override void OnUpdate(){
-            if(DisplayDict.Count!=0){
-                foreach(var proto in DisplayDict.Values)uObject.Destroy(proto.gameObject);
-                DisplayDict.Clear();
+        [HarmonyPatch(typeof(Weapon),"SpawnDart")]
+        public class WeaponSpawnDart_Patch{
+            [HarmonyPostfix]
+            public static void Postfix(Weapon __instance){
+                string towerName=__instance.attack.tower.towerModel.baseId;
+                if(TowerTypes.ContainsKey(towerName)){
+                    TowerTypes[towerName].Attack(__instance);
+                }
             }
         }
-        public static readonly ModSettingBool ProtossEnabled=true;
-        public static readonly ModSettingBool TerranEnabled=true;
-        public static readonly ModSettingBool ZergEnabled=true;
-        public static readonly ModSettingBool RemoveBaseTowers=false;
-        public static readonly ModSettingBool HeroesEnabled=true;
-        public static Dictionary<string,UnityDisplayNode>DisplayDict=new();
-        public static AudioFactory AudioFactoryInstance;
-        public class ProtossSet:ModTowerSet{
-            public override string DisplayName=>"Protoss";
-            public override string Container=>"ProtossContainer";
-            public override string Button=>"ProtossButton";
-            public override string ContainerLarge=>Container;
-            public override string Portrait=>"ProtossHex";
+        [HarmonyPatch(typeof(Tower),"OnPlace")]
+        public class TowerOnPlace_Patch{
+            [HarmonyPostfix]
+            public static void Postfix(Tower __instance){
+                string towerName=__instance.towerModel.baseId;
+                if(TowerTypes.ContainsKey(towerName)){
+                    TowerTypes[towerName].Create();
+                }
+            }
         }
-        public class TerranSet:ModTowerSet{
-            public override string DisplayName=>"Terran";
-            public override string Container=>"TerranContainer";
-            public override string Button=>"TerranButton";
-            public override string ContainerLarge=>Container;
-            public override string Portrait=>"TerranPortrait";
+        [HarmonyPatch(typeof(Ability),"Activate")]
+        public class AbilityActivate_Patch{
+            [HarmonyPostfix]
+            public static void Postfix(Ability __instance){
+                string towerName=__instance.tower.towerModel.baseId;
+                if(TowerTypes.ContainsKey(towerName)){
+                    TowerTypes[towerName].Ability(__instance.abilityModel.name,__instance.tower);
+                }
+            }
         }
-        public class ZergSet:ModTowerSet{
-            public override string DisplayName=>"Zerg";
-            public override string Container=>"ZergContainer";
-            public override string Button=>"ZergButton";
-            public override string ContainerLarge=>Container;
-            public override string Portrait=>"ZergCreep";
+        [HarmonyPatch(typeof(AudioFactory),nameof(AudioFactory.Start))]
+        public class AudioFactoryStart_Patch{
+            [HarmonyPostfix]
+            public static void Postfix(AudioFactory __instance){
+                foreach(string bundlePath in Directory.GetFiles(BundleDir)){
+                    if(bundlePath.EndsWith("clips")){
+                        AssetBundleRequest bundle=AssetBundle.LoadFromFileAsync(bundlePath).assetBundle.LoadAllAssetsAsync<AudioClip>();
+                        foreach(uObject asset in bundle.allAssets){
+                            __instance.RegisterAudioClip(asset.name,asset.Cast<AudioClip>());
+                        }
+                    }
+                }
+            }
+        }
+        [HarmonyPatch(typeof(TowerManager),"UpgradeTower")]
+        public class TowerManagerUpgradeTower_Patch{
+            [HarmonyPostfix]
+            public static void Postfix(Tower tower,int pathIndex){
+                string towerName=tower.towerModel.baseId;
+                if(TowerTypes.ContainsKey(towerName)){
+                    TowerTypes[towerName].Upgrade(pathIndex,tower);
+                }
+            }
+        }
+        public static void PlaySound(string name){
+            Game.instance.audioFactory.PlaySoundFromUnity(null,name,"FX",1,1);
+        }
+        public static void PlayAnimation(UnityDisplayNode udn,string anim){
+            udn.GetComponent<Animator>().Play(anim);
+        }
+        [HarmonyPatch(typeof(TowerSelectionMenu),"SelectTower")]
+        public class TowerSelectionMenuSelectTower_Patch{
+            [HarmonyPostfix]
+            public static void Postfix(TowerSelectionMenu __instance){
+                string towerName=__instance.selectedTower.tower.towerModel.baseId;
+                if(TowerTypes.ContainsKey(towerName)){
+                    TowerTypes[towerName].Select(__instance.selectedTower.tower);
+                }
+            }
         }
     }
 }
---
+++ SC2Tower.cs
@@ -0,0 +1,47 @@
+﻿namespace SC2ExpansionLoader{
+    public class SC2Tower{
+        public virtual string Name=>"";
+        public virtual byte[]TowerBundle=>null;
+        public virtual ShopTowerDetailsModel ShopDetails()=>null;
+        public virtual TowerModel[]TowerModels()=>null;
+        public virtual UpgradeModel[]Upgrades()=>null;
+        public AssetBundle LoadedBundle=null;
+        public virtual void Attack(Weapon weapon){}
+        public virtual void Upgrade(int tier,Tower tower){}
+        public virtual void Ability(string ability,Tower tower){}
+        public virtual void Create(){}
+        public virtual void Select(Tower tower){}
+        public virtual void Sell(Tower tower){}
+        public virtual Dictionary<string,string>SoundNames=>null;
+    }
+    [RegisterTypeInIl2Cpp]
+    public class SC2Sound:MonoBehaviour{
+        public SC2Sound(IntPtr ptr):base(ptr){}
+        public int LastSelectQuote=0;
+        public int LastUpgradeQuote=0;
+        public int MaxSelectQuote=7;
+        public int MaxUpgradeQuote=5;
+        public string TowerName;
+        public void Start(){
+            string[]name=gameObject.name.Split('-');
+            try{
+                TowerName=TowerTypes[name[0]].SoundNames[name[1]];
+            }catch{}
+        }
+        public void PlaySelectSound(){
+            LastSelectQuote++;
+            if(LastSelectQuote==MaxSelectQuote){
+                LastSelectQuote=1;
+            }
+            PlaySound(TowerName+"Select"+LastSelectQuote);
+        }
+        public void PlayUpgradeSound(){
+            LastUpgradeQuote++;
+            if(LastUpgradeQuote==MaxUpgradeQuote){
+                LastUpgradeQuote=1;
+            }
+            LastSelectQuote=1;
+            PlaySound(TowerName+"Upgrade"+LastUpgradeQuote);
+        }
+    }
+}
