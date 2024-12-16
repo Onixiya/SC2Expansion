955d134ef506894241028f2e03ef63389d9b98cb
silentstorm <29950414+Onixiya@users.noreply.github.com>
2024-12-17 10:59:10 +1100

    bump version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,8 +1,8 @@
 namespace SC2ExpansionLoader.Data{
     public static class ModHelperData{
         public const string WorksOnVersion="46";
-        public const string Version="3.2.0";
-        public const string Name="SC2ExpansionLoader";
+        public const string Version="3.2.1";
+        public const string Name="SC2Expansion";
         public const string Description="Loader for any SC2Expansion mods, this mod adds nothing by itself. Will be automatically downloaded if required";
         public const string RepoOwner="Onixiya";
         public const string RepoName="SC2Expansion";
